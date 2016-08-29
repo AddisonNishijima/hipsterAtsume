dbb873549a328841a1a7edd7ed785e3a98592bba
Ewa Manek and Francis Prus <ewa@>
2016-08-29 09:56:11 -0700

    Create objects and prototypes in Javascripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
 //<!-- Back End -->
+function Item(type) {
+  this.type = type;
+  this.inInventory = false;
+  this.displayed = false;
+}
+
+Item.prototype.toggleInventory = function(){
+  this.inInventory = !this.inInventory;
+  if(!this.inInventory && this.displayed){
+    this.displayed = false;
+  }
+}
+
+Item.prototype.toggleDisplay = function(){
+  if(this.inInventory){
+    this.displayed = !this.displayed;
+  }
+  return this.displayed;
+}
+
+function Hipster(type, likedItems, dislikedItems) {
+  this.type = type;
+  this.likedItems = likedItems;
+  this.dislikedItems =  dislikedItems;
+  this.affinityMeter = 0;
+}
+
+Hipster.prototype.checkAffinity = function(displayedItems){
+  for(var i = 0; i < displayedItems.length; i++){
+    if(this.likedItems.indexOf(displayedItems[i].type) !== -1){
+      this.affinityMeter++;
+    } else if(this.dislikedItems.indexOf(displayedItems[i].type) !== -1){
+      this.affinityMeter--;
+    }
+  }
+  return this.affinityMeter;
+}
 
 
 //<!-- Front End  -->
